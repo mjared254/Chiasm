35bda8b18de6ae552a18366326d959c936768625
mjared254 <158103715+mjared254@users.noreply.github.com>
2026-08-20 00:34:20 -0700

    Collecting Annotation tag & box labels

--- src/lisa_extract.py
+++ src/lisa_extract.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 
 #Filepath to LisaDataset Folder
 FILE_PATH = '/Users/jaredmorales04/Chiasm/LisaDataset'
@@ -15,9 +16,10 @@ class LISADataset():
         self.file_path = file_path
         self.transforms = transforms
         self.samples = []
-
+        #when we create LISADataset Object, we pass clip_names two filenames.
         print(f"Loading Annotations from {clip_names}")
-    #
+
+        #loads  each clip in the clip_names list, when LISADataset object is created
         for clip_name in clip_names:
             self.load_clip(clip_name)
 
@@ -25,16 +27,51 @@ class LISADataset():
 
         if len(self.samples) == 0:
             raise ValueError("No images were loaded, Check Dataset Strcuture")
-        
+
+
+    #Dataset Strucutre
+    #LisaDataset / Annotations / Annotations / daySequence1 / frameAnnotation-BOX.csv 
     def load_clip(self, clip_name):
+        #sets the path to the Annotations / Annotations / clip_name
         anno_file_path = os.path.join(FILE_PATH + "Annotations" + "Annotations" + clip_name)
 
         if not os.path.exists(anno_file_path):
             print(f"Annotations directory not found: {anno_file_path}")
 
-            sub_clips = [d for d in os.listdir(anno_file_path) if os.path.isdir(os.path.join(anno_file_path, d))]
+            return
 
-            print(f"Processing {clip_name}: found {len(sub_clips)} subclips")
+        sub_clips = [d for d in os.listdir(anno_file_path) if os.path.isdir(os.path.join(anno_file_path, d))]
 
-            for sub_clip in sorted(sub_clips):
-                csv_path = os.path.join(anno_file_path, sub_clip, 'frameAnnotation-BOX.csv')
+        print(f"Processing {clip_name}: found {len(sub_clips)} subclips")
+
+        for sub_clip in sorted(sub_clips):
+            csv_path = os.path.join(anno_file_path, sub_clip, 'frameAnnotation-BOX.csv')
+
+            if not os.path.exists(csv_path):
+                continue
+
+            df = pd.read_csv(csv_path)
+            #process each file in the dataframe, checks each one is different
+            #never processes the same one
+            for filename in df['Filname'].unique():
+                image_annotations = df[df['Filename'] == filename]
+
+                boxes = []
+                labels = []
+                #iterrows, returns index, row data as a PandaSeries
+                for _, row in image_annotations.iterrows():
+                    x1 = float(row['Upper left corner X'])
+                    y1 = float(row['Upper left corner Y'])
+                    x2= float(row['Lower right corner X'])
+                    y2= float(row['Lower right corner Y'])
+
+                    if x2 <= x1 or y2 <= y1:
+                        continue
+
+                    annotations_label = str(row['Annotation tag']).tolower()
+
+                    label = self.parese_label(annotations_label)
+
+                    boxes.append([x1,y1,x2,y2])
+
+                    labels.append(label)
